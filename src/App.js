64414d7426857e5a556fdad2839377e240b33261
Param4895 <paramjeet.kaur@mail.vinove.com>
2019-12-18 18:34:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <Map google={this.props.google}
+        style={{ width: '100%', height: '100%', position: 'relative' }}
+        className={'map'}
+        zoom={14}>
+        <Marker
+          title={'The marker`s title will appear as a tooltip.'}
+          name={'ValueCoders'}
+          position={{ lat: 28.4528, lng: 77.0697 }} />
+        <Marker
+          name={'Sector 40'}
+          position={{ lat: 28.4498, lng: 77.0567 }} />
+        <Marker />
+        <Marker
+          name={'Amritsar'}
+          position={{ lat: 31.6340, lng: 74.8723 }}
+        />
+        <Marker
+          name={'Pune'}
+          position={{ lat: 18.5204, lng: 73.8567 }}
+        />
+        <Marker
+          name={'Guwahati'}
+          position={{ lat: 26.1445, lng: 91.7362 }}
+        />
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDCJOIA3sGdG37ObmUBsBO3dK3aXfkUuLQ")
+})(App)
